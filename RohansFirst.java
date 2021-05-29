d963896fac5abd2ed4b9648b9d2ba509658eb315
CobaltCabana <68398596+CobaltCabana@users.noreply.github.com>
2021-05-29 01:19:43 -0500

    Add files via upload
    SecondPass with For Loop

--- RohansFirst.java
+++ RohansFirst.java
@@ -4,16 +4,23 @@ public class RohansFirst {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Number of arguements = " + args.length);
-		if (args.length > 2) {
-			System.out.println("Where is the food? " + args[0] + " " + args[1] + " " + args[2]);
-		} else if (args.length > 1) {
-			System.out.println("Where is the food? " + args[0] + " " + args[1]);
-		} else if (args.length > 0) {
-			System.out.println("Where is the food? " + args[0]);
-		} else {
-			System.out.println("You didn't give me any arguements!");
+		
+		var len = args.length;
+		
+		for(var i = -1; i < len; i++) {
+			System.out.println("Number of arguements = " + i);
+			if (i >= 2) {
+				System.out.println("Where is the food? " + args[0] + " " + args[1] + " " + args[2]);
+			} else if (i >= 1) {
+				System.out.println("Where is the food? " + args[0] + " " + args[1]);
+			} else if (i >= 0) {
+				System.out.println("Where is the food? " + args[0]);
+			} else if (i >= -1){
+				System.out.println("You didn't give me any arguements!");
+			}
+			
+			}
 		}
+	
 	}
 
-}
