b45256e63be8c32fbc2259d7404a78badfcb7834
CobaltCabana <68398596+CobaltCabana@users.noreply.github.com>
2021-05-31 22:01:22 -0500

    Add files via upload
    The new version, which uses For Loops, If Statements

--- RohansFirst.java
+++ RohansFirst.java
@@ -1,26 +1,25 @@
 package example1;
-
+/**
+ * Learning about command line arguments, for loops, and if statements
+ * @author rohan
+ *
+ */
 public class RohansFirst {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		var len = args.length;
-		
-		for(var i = -1; i < len; i++) {
-			System.out.println("Number of arguements = " + i);
-			if (i >= 2) {
-				System.out.println("Where is the food? " + args[0] + " " + args[1] + " " + args[2]);
-			} else if (i >= 1) {
-				System.out.println("Where is the food? " + args[0] + " " + args[1]);
-			} else if (i >= 0) {
-				System.out.println("Where is the food? " + args[0]);
-			} else if (i >= -1){
-				System.out.println("You didn't give me any arguements!");
-			}
-			
+
+		String s = "";
+		for (var i = 0; i < len; i++) {
+			// System.out.print("Args[" + i + "] = " + args[i]);
+			if (i == (len - 1)) {
+				s = s + args[i] + ".";
+			} else {
+				s = s + args[i] + ", ";
 			}
 		}
-	
+		System.out.println(s);
 	}
-
+}
